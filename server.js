f0c30c576b8e1f52704d6acd2c9e5c4f8b3d106f
Liraz Gur Arie <lirazgurarie@walla.com>
2018-12-04 22:13:14 +0200

    prepared for deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const users = require('./routes/api/users');
 const admin = require('./routes/api/admin');
@@ -29,6 +30,15 @@ require('./config/passport')(passport);
 app.use('/api/admin',admin);
 app.use('/api/users',users);
 
+//serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+  //set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000 ;
 
