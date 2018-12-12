078a7e71a1e60b378d6a83d80292a5a65a8ce9f8
Liraz Gur Arie <lirazgurarie@walla.com>
2018-12-12 16:30:28 +0200

    improved code at seperator

--- client/src/components/seperator.js
+++ client/src/components/seperator.js
@@ -2,16 +2,8 @@ import React from 'react';
 import LazyLoad from 'react-lazy-load';
 
 const Seperator = (props) => {
-  const isIE = /*@cc_on!@*/false || !!document.documentMode;
-  const isEdge = !isIE && !!window.StyleMedia;
-  let className;
-  if(isEdge || isIE){
-    className = 'seperator seperator__height u-margin-top-big u-margin-bottom-big';
-  } else {
-    className = 'seperator u-margin-top-big u-margin-bottom-big';
-  }
   return (
-    <LazyLoad offsetVertical={700} debounce={false} className={className}>
+    <LazyLoad offsetVertical={700} debounce={false} className='seperator u-margin-top-big u-margin-bottom-big'>
       <div className="seperator--img" style={{ backgroundImage: `url(${props.bgimage})`,backgroundPosition:`${props.bgposition}`}}></div>
     </LazyLoad>
   );
